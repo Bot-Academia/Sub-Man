162c99bce5f846f18f468856ef2708fd8fd02046
codetheorem <hrishikeshagarwalv@gmail.com>
2020-07-01 14:32:54 +0530

    added header

---
+++ src/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="header">
+<b-navbar toggleable="lg" type="light">
+      <b-navbar-brand href="#" id="navbar-brand" @click="change1('apphome')"
+        >Subman</b-navbar-brand
+      >
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav>
+            <b-nav-item>Pied_Piper</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+    </b-navbar>
+</div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+#header{
+    background-color: transparent;
+    color: black;
+}
+
+</style>
