7754d945107ad55077814436b073221a3677eb6f
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2020-07-01 15:03:17 +0530

    Add files via upload

---
+++ src/views/login.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <app-header></app-header>
+    <br>
+    login
+</div>
+</template>
+
+
+<script>
+import header from "../components/header";
+export default {
+    components: {
+        appHeader: header
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
